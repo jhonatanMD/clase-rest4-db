43bf95b9c716228a7e89963e6d5097067f1a7a12
10038045 <jmdiaz190899@gmail.com>
2022-10-26 20:58:17 -0500

    se agrega nuevo codigo para conectar a bd y haciendo query select * from clientes

--- src/main/java/com/ws/repository/IBDRepository.java
+++ src/main/java/com/ws/repository/IBDRepository.java
@@ -2,11 +2,12 @@ package com.ws.repository;
 
 import com.ws.model.ClienteEntity;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IBDRepository {
 
-    List<ClienteEntity> findAll();
+    List<ClienteEntity> findAll() throws SQLException;
     ClienteEntity save();
 
 }
--- src/main/java/com/ws/repository/impl/BDRepository.java
+++ src/main/java/com/ws/repository/impl/BDRepository.java
@@ -4,10 +4,14 @@ import com.ws.model.ClienteEntity;
 import com.ws.repository.IBDRepository;
 import io.agroal.api.AgroalDataSource;
 import io.quarkus.agroal.DataSource;
-import lombok.RequiredArgsConstructor;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
@@ -15,11 +19,32 @@ public class BDRepository implements IBDRepository {
 
     @Inject
     @DataSource("prueba")
-    private AgroalDataSource cnn;
+    private AgroalDataSource dataSource;
+
 
     @Override
-    public List<ClienteEntity> findAll() {
-        return null;
+    public List<ClienteEntity> findAll() throws SQLException {
+
+        List<ClienteEntity> clientes = new ArrayList<>();
+
+        Connection cnn = dataSource.getConnection();
+        Statement statement = cnn.createStatement();
+
+        ResultSet rs = statement.executeQuery("SELECT * FROM cliente");
+
+        while (rs.next()){
+            clientes.add(ClienteEntity.builder()
+                            .id(rs.getLong(1))
+                            .nombre(rs.getString(2))
+                            .apellido(rs.getString(3))
+                            .edad(rs.getLong(4))
+                    .build());
+        }
+
+        statement.close();
+        cnn.close();
+
+        return clientes;
     }
 
     @Override
